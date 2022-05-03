14c1a8ca1918410befcd1f27150fa7bb6fbcbcde
boris-minsk <boris_sigal@earthlink.net>
2022-05-03 15:01:53 +0200

    Explanation comments added to the GenericList.

--- GenericList.cs
+++ GenericList.cs
@@ -2,10 +2,18 @@
 
 namespace MmGuideAssignment
 {
+    /*
+     Replace the [AnimalList and DoubleList] lists with a GenericList class with a single DoSomething() method.
+    */
     public class GenericList<T>
     {
         public void DoSomething(T param)
         {
+            /* I don't like an idea to switch param type to print an appropriate method.
+            But DoSomething() method just prints the message based on param type, so I did.
+            Or I missed something?
+            Boris Sigal */
+
             if (param is double)
             {
                 Console.WriteLine("Foo Number");
